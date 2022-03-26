85d59f2c39a48ed3c364ec4fab36bfb084563689
AugustoThiago <thiago.augusto.alvarenga@gmail.com>
2022-03-25 22:08:23 -0300

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,25 +5,28 @@ import { Component, VERSION } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: [ './app.component.css' ]
 })
-export class AppComponent  {
+export class AppComponent {
   title = 'TODO List Angular';
   
   list:any[]=[];
-  addTask(item:string){
-    this.list.push({id:this.list.length, name:item});
-  }
-  removeTask(id:number){
-    this.list = this.list.filter(item=>item.id!==id);
-  }
-
-  saveOnLocalStorage() {
+  saveStorage() {
     const data = JSON.stringify(this.list);
     localStorage.setItem('todos', data);
   }
-
-  loadFromLocalStorage() {
+  
+  loadStorage() {
     const data = localStorage.getItem('todos');
     this.list = JSON.parse(data);
+  }
+  
+  addTask(item:string){
+    this.list.push({id:this.list.length, name:item});
+    this.saveStorage();
+  }
+
+  removeTask(id:number){
+    this.list = this.list.filter(item=>item.id!==id);
+    this.saveStorage();
   }
 
 }
