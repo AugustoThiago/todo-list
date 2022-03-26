05fbdffc3f7f64be32be2cc775d362ac720d9921
AugustoThiago <thiago.augusto.alvarenga@gmail.com>
2022-03-25 21:31:07 -0300

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,17 @@ import { Component, VERSION } from '@angular/core';
   styleUrls: [ './app.component.css' ]
 })
 export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+  title = 'TODO List Angular';
+  
+  list:any[]=[];
+  addTask(item:string)
+  {
+    this.list.push({id:this.list.length, name:item})
+    // console.warn(this.list);
+  }
+  removeTask(id:number)
+  {
+    // console.warn(id)
+    this.list = this.list.filter(item=>item.id!==id);
+  }
 }
